c706c213744408379580991baeecc8904015a14a
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-04-22 16:23:09 +0100

    Feat: Added tailwindcss

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("Script connected successfully");
+const params = new URLSearchParams(window.location.search);
+
+const paramsObject = {};
+
+for (let [key, value] of params.entries()) {
+  paramsObject[key] = value;
+}
+
+console.log(paramsObject);
+
+
+
+const fetchBlob = () => {
+  
+}

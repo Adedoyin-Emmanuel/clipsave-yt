f5aa8105a7409c26f8dbf480b6cad93a68a91037
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-05-05 06:40:58 +0100

    Feat: Connected frontend to backend

--- script.js
+++ script.js
@@ -1,26 +1,156 @@
+const formatNumber = (count) => {
+  if (count < 1000) {
+    return count.toString();
+  } else if (count < 1000000) {
+    return (count / 1000).toFixed(1) + "K";
+  } else if (count < 1000000000) {
+    return (count / 1000000).toFixed(1) + "M";
+  } else {
+    return (count / 1000000000).toFixed(1) + "B";
+  }
+};
+
+const renderData = (data, $) => {
+  const {
+    id,
+    likes,
+    dislikes,
+    rating,
+    viewCount,
+    deleted,
+    thumbnailUrl,
+    title,
+    a,
+    links,
+    related,
+  } = data;
+
+  const imageRef = $("#ytThumbnail");
+  imageRef.attr("src", thumbnailUrl);
+  $("#likes").text(formatNumber(likes));
+  $("#dislikes").text(formatNumber(dislikes));
+  $("#views").text(formatNumber(viewCount));
+  $("#deleted").text(deleted ? "Yes " : "No");
+  $("#rating").text(formatNumber(Math.ceil(rating)));
+
+  $("#videoTitle").html(`
+         <p>
+       ${title}
+        </p>
+        <p class="text-sm">${a.includes("@") ? a : "@" + a}</p>
+    `);
+
+  renderVideoTable(links, $);
+};
+
+const renderVideoTable = (links, $) => {
+  const { mp4, mp3, other } = links;
+
+  const mp4Entry = Object.entries(mp4);
+  const mp3Entry = Object.entries(mp3);
+  const otherEntry = Object.entries(other);
+  const mp4TableBody = $("#mp4TableBody");
+  const mp3TableBody = $("#mp3TableBody");
+  const othersTableBody = $("#othersTableBody");
+
+  mp4TableBody.empty();
+  mp3TableBody.empty();
+  othersTableBody.empty();
+
+  mp4Entry.forEach(([key, entry]) => {
+    mp4TableBody.append(`
+            <tr>
+                <td class="px-6 py-3 text-start">${entry.q_text}</td>
+                <td class="px-6 py-3 text-start">${entry.size || "Unknown"}</td>
+                <td class="px-6 py-3 text-start">
+                    <button class="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 text-[14px]">
+                        Download
+                    </button>
+                </td>
+            </tr>
+        `);
+  });
+
+  mp3Entry.forEach(([key, entry]) => {
+    mp3TableBody.append(`
+            <tr>
+                <td class="px-6 py-3 text-start">${entry.q_text}</td>
+                <td class="px-6 py-3 text-start">${entry.size || "Unknown"}</td>
+                <td class="px-6 py-3 text-start">
+                    <button class="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 text-[14px]">
+                        Download
+                    </button>
+                </td>
+            </tr>
+        `);
+  });
+
+  otherEntry.forEach(([key, entry]) => {
+    othersTableBody.append(`
+            <tr>
+                <td class="px-6 py-3 text-start">${entry.q_text}</td>
+                <td class="px-6 py-3 text-start">${entry.size || "Unknown"}</td>
+                <td class="px-6 py-3 text-start">
+                    <button class="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 text-[14px]">
+                        Download
+                    </button>
+                </td>
+            </tr>
+        `);
+  });
+};
+
 jQuery(($) => {
   $.noConflict();
 
-  const handleSubmit = () => {
-    console.log("Hello world");
+  const ANALYZE_URL = "http://localhost:3000/api/analyze";
+  const DOWNLOAD_URL = "http://localhost:3000/api/downloader/youtube";
+
+  let isLoading = false;
+
+  const handleConvert = () => {};
+
+  const handleSubmit = (value) => {
+    const youtubeLink = value;
+
+    if (!isLoading) {
+      isLoading = true;
+    }
+
+    $.ajax({
+      url: ANALYZE_URL,
+      type: "POST",
+      data: JSON.stringify({
+        url: youtubeLink,
+      }),
+      success: (response) => {
+        console.log(response);
+        isLoading = false;
+
+        renderData(response.data, $);
+      },
+      error: (xhr, status, error) => {
+        alert(error);
+        isLoading = false;
+      },
+    });
   };
 
-  $("#youtubeLinkInput").on("paste", function (event) {
-    event.preventDefault();
+  $("#youtubeLinkInput").on("paste", (e) => {
+    e.preventDefault();
 
-    const pastedText = (event.originalEvent || event).clipboardData.getData(
-      "text"
-    );
+    const pastedText = (e.originalEvent || e).clipboardData.getData("text");
+    const input = $("#youtubeLinkInput");
 
-    console.log("Pasted text:", pastedText);
-    handleSubmit();
+    input.val(pastedText);
+
+    handleSubmit(pastedText);
   });
 
-  $("#submitButton").on("click", function () {
+  $("#form").on("submit", (e) => {
+    e.preventDefault();
     const inputValue = $("#youtubeLinkInput").val();
 
-    console.log("Submitted value:", inputValue);
-
-    handleSubmit();
+    handleSubmit(inputValue);
   });
 });
